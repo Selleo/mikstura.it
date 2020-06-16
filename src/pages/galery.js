e665335aca5a4f988e6c7f6fb5eca5656d5ba525
Jakub Brożek <jakub.brozek17@gmail.com>
2020-06-16 16:09:03 +0200

    subpages added

---
+++ src/pages/galery.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Link } from "gatsby"
+import styled from 'styled-components'
+import Header from '../components/header'
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+import Navigation from '../components/navigation'
+
+const StyledWrapper = styled.div`
+  background-color: white;
+  color: #404040;
+` ;
+
+
+const IndexPage = () => (
+  <StyledWrapper>
+    <Header>mikstura<font color="orange">.IT</font>
+      <Navigation />
+    </Header>
+    <h3>To jest strona poświęcona galerii.</h3>
+  </StyledWrapper>
+)
+
+export default IndexPage
