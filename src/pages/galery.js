19c55f754a2b3de81c1085bc38272538de82f2f8
Jakub Brożek <jakub.brozek17@gmail.com>
2020-06-24 12:14:01 +0200

    awesome custom scrollbar

--- src/pages/galery.js
+++ src/pages/galery.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Layout from '../components/layout';
-import "../styles/normalize.css";
-import "../styles/stylesheet.css";
 
 const IndexPage = () => (
   <Layout />
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from '../components/layout';
 import "../styles/normalize.css";
 import "../styles/stylesheet.css";
+import "../styles/scrollbar.css";
 
 const IndexPage = () => (
   <Layout />
