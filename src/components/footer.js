068926b06021bb03f3ab2e0bc4ad2642553b481c
Jakub Brożek <jakub.brozek17@gmail.com>
2020-06-24 10:44:06 +0200

    bug fixes

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,28 +8,28 @@ const Footer = () => (
       <a href="http://mikstura.it" className='footer-link'>mikstura<span className="header-link__it">.IT</span></a>
     </div>
     <ol className='footer__icon'>
-      <a href="https://www.meetup.com/Mikstura-IT/" className='footer-link' activeClassName='active'><i class="fab fa-meetup"></i></a>
+      <a href="https://www.meetup.com/Mikstura-IT/" className='footer-link'><i class="fab fa-meetup"></i></a>
     </ol>
     <ol className='footer__icon'>
-      <a href="https://miksturait.slack.com/" className='footer-link' activeClassName='active'><i class="fab fa-slack"></i></a>
+      <a href="https://miksturait.slack.com/" className='footer-link'><i class="fab fa-slack"></i></a>
     </ol>
     <ol className='footer__icon'>
-      <a href="https://www.youtube.com/user/MiksturaIT" className='footer-link' activeClassName='active'><i class="fab fa-youtube"></i></a>
+      <a href="https://www.youtube.com/user/MiksturaIT" className='footer-link'><i class="fab fa-youtube"></i></a>
     </ol>
     <ol className='footer__icon'>
-      <a href="https://github.com/miksturait" className='footer-link' activeClassName='active'><i class="fab fa-github"></i></a>
+      <a href="https://github.com/miksturait" className='footer-link'><i class="fab fa-github"></i></a>
     </ol>
     <ol className='footer__icon'>
-      <a href="https://facebook.com/MiksturaIT" className='footer-link' activeClassName='active'><i class="fab fa-facebook"></i></a>
+      <a href="https://facebook.com/MiksturaIT" className='footer-link'><i class="fab fa-facebook"></i></a>
     </ol>
     <ol className='footer__icon'>
-      <a href="https://twitter.com/miksturait" className='footer-link' activeClassName='active'><i class="fab fa-twitter"></i></a>
+      <a href="https://twitter.com/miksturait" className='footer-link'><i class="fab fa-twitter"></i></a>
     </ol>
     <ol className='footer__icon'>
-      <a href="https://linkedin.com/company/mikstura-it-foundation" className='footer-link' activeClassName='active'><i class="fab fa-linkedin"></i></a>
+      <a href="https://linkedin.com/company/mikstura-it-foundation" className='footer-link'><i class="fab fa-linkedin"></i></a>
     </ol>
     <ol className='footer__icon'>
-      <a href="mailto:fundacja@mikstura.it" className='footer-link' activeClassName='active'><i class="far fa-envelope"></i></a>
+      <a href="mailto:fundacja@mikstura.it" className='footer-link' ><i class="far fa-envelope"></i></a>
     </ol>
   </li>
 );
