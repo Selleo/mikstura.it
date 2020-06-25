0aa95c01cfde6ea93d6ed5efb951d7c926a39857
Jakub Brożek <jakub.brozek17@gmail.com>
2020-06-25 11:38:23 +0200

    broken commit rework

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import Layout from '../components/layout';
 import "../styles/normalize.css";
 import "../styles/stylesheet.css";
-import "../styles/scrollbar.css";
 
 const IndexPage = () => (
   <Layout />
