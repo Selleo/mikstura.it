d433ef5da0537d55ac023f09e7e5ecee13699d65
wozny1989 <wozny1989@gmail.com>
2020-06-26 14:17:15 +0200

    Resolve problems with bugs

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,32 +7,32 @@ const Footer = () => (
     <li className='footer-link-wrapper'>
       <div className='footer__logo'></div>
       <ol className='footer__icon'>
-        <a href="https://www.meetup.com/Mikstura-IT/" className='footer-link'><i class="fab fa-meetup"></i></a>
+        <a href="https://www.meetup.com/Mikstura-IT/" className='footer-link'><i className="fab fa-meetup"></i></a>
       </ol>
       <ol className='footer__icon'>
-        <a href="https://miksturait.slack.com/" className='footer-link'><i class="fab fa-slack"></i></a>
+        <a href="https://miksturait.slack.com/" className='footer-link'><i className="fab fa-slack"></i></a>
       </ol>
       <ol className='footer__icon'>
-        <a href="https://www.youtube.com/user/MiksturaIT" className='footer-link'><i class="fab fa-youtube"></i></a>
+        <a href="https://www.youtube.com/user/MiksturaIT" className='footer-link'><i className="fab fa-youtube"></i></a>
       </ol>
       <ol className='footer__icon'>
-        <a href="https://github.com/miksturait" className='footer-link'><i class="fab fa-github"></i></a>
+        <a href="https://github.com/miksturait" className='footer-link'><i className="fab fa-github"></i></a>
       </ol>
       <ol className='footer__icon'>
-        <a href="https://facebook.com/MiksturaIT" className='footer-link'><i class="fab fa-facebook"></i></a>
+        <a href="https://facebook.com/MiksturaIT" className='footer-link'><i className="fab fa-facebook"></i></a>
       </ol>
       <ol className='footer__icon'>
-        <a href="https://twitter.com/miksturait" className='footer-link'><i class="fab fa-twitter"></i></a>
+        <a href="https://twitter.com/miksturait" className='footer-link'><i className="fab fa-twitter"></i></a>
       </ol>
       <ol className='footer__icon'>
-        <a href="https://linkedin.com/company/mikstura-it-foundation" className='footer-link'><i class="fab fa-linkedin"></i></a>
+        <a href="https://linkedin.com/company/mikstura-it-foundation" className='footer-link'><i className="fab fa-linkedin"></i></a>
       </ol>
       <ol className='footer__icon'>
-        <a href="mailto:fundacja@mikstura.it" className='footer-link' ><i class="far fa-envelope"></i></a>
+        <a href="mailto:fundacja@mikstura.it" className='footer-link' ><i className="far fa-envelope"></i></a>
       </ol>
     </li>
     <div className='footer__bottomline'></div>
-    <div className='footer-credits'>© {new Date().getFullYear()} Selleo</div>  
+    <div className='footer-credits'>© {new Date().getFullYear()} Selleo</div>
   </div>
 );
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,32 +1,14 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
 import React from "react";
-import PropTypes from "prop-types";
-import { useStaticQuery, graphql } from "gatsby";
 import { Link } from 'gatsby';
 import Navigation from './navigation';
 import Footer from "./footer";
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-
   return (
     <>
       <div className='header'>
         <div className='header-logo'>
-          <Link className='header-link' to={/./}>mikstura<span className="header-link__it">.IT</span></Link>
+          <Link className='header-link' to={'/'}>mikstura<span className="header-link__it">.IT</span></Link>
         </div>
         <Navigation />
       </div>
@@ -36,10 +18,6 @@ const Layout = ({ children }) => {
       <Footer />
     </>
   );
-};
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 };
 
 export default Layout;
